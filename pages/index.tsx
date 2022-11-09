2d4f8696869ade91ea50af817b921d44b88205d3
tu.le1 <tu.le1@ntq-solution.com.vn>
2022-11-09 09:10:47 +0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <p>test commit</p>
         </h1>
 
         <p className={styles.description}>
